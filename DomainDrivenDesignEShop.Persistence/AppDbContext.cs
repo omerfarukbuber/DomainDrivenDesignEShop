2edf55dd656fe7eabd4db17d2cd9a41075218eec
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-08-09 21:57:50 +0300

    Create application db context class and override methods

---
+++ DomainDrivenDesignEShop.Persistence/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainDrivenDesignEShop.Persistence;
+
+public class AppDbContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseNpgsql();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+    }
+}
