98d7f8168b62ec0f9ce7cf4a95b835da36863106
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-08-09 22:00:09 +0300

    Create order configuration file and relate entities

---
+++ DomainDrivenDesignEShop.Persistence/Configurations/OrderConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using DomainDrivenDesignEShop.Domain.Customers;
+using DomainDrivenDesignEShop.Domain.Orders;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace DomainDrivenDesignEShop.Persistence.Configurations;
+
+internal class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.Id).HasConversion(
+            orderId => orderId.Value,
+            value => new OrderId(value));
+
+        builder.HasOne<Customer>()
+            .WithMany()
+            .HasForeignKey(o => o.CustomerId)
+            .IsRequired();
+
+        builder.HasMany(o => o.LineItems)
+            .WithOne()
+            .HasForeignKey(li => li.OrderId);
+    }
+}
