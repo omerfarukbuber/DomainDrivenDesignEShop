858de60bdc1c86f808f28d4a5f463a9e2b5e750b
Ömer Faruk BÜBER <omerfbuber@gmail.com>
2024-08-10 12:27:59 +0300

    CreateOrderCommand feature with Mediatr

---
+++ DomainDrivenDesignEShop.Application/Orders/Create/CreateOrderCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using DomainDrivenDesignEShop.Application.Data;
+using DomainDrivenDesignEShop.Domain.Customers;
+using DomainDrivenDesignEShop.Domain.Orders;
+using MediatR;
+
+namespace DomainDrivenDesignEShop.Application.Orders.Create;
+
+internal sealed class CreateOrderCommandHandler(IAppDbContext context) : IRequestHandler<CreateOrderCommand>
+{
+    private readonly IAppDbContext _context = context;
+
+    public async Task Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+    {
+        var customer = await _context.Customers.FindAsync(request.CustomerId, cancellationToken);
+
+        if (customer is null)
+        {
+            return;
+        }
+
+        var order = Order.Create(customer.Id);
+        await _context.Orders.AddAsync(order, cancellationToken);
+        await _context.SaveChangesAsync(cancellationToken);
+    }
+}
